74eb946daaf861aba6e7a5ee76729cc30ae09609
Kaitlyn Concilio <git@kait.email>
2026-03-07 09:53:50 -0800

    Update setup script version to 1.5.0

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ set -e
 
 # Swoops Interactive Setup Script
 # This script guides you through configuring Swoops for production deployment
-SETUP_SCRIPT_VERSION="1.4.0"
+SETUP_SCRIPT_VERSION="1.5.0"
 
 # Parse command line arguments for non-interactive agent setup
 NON_INTERACTIVE=false
